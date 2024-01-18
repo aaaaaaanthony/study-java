790bf842fb957c7bd315bfcd1a88a4dd88d86607
Anthony <runtimes.cc@gmail.com>
2024-01-18 17:23:04 +0400

    1

--- springboot-redis/redission-demo/src/main/java/com/example/redissondemo/RedissonDemoApplication.java
+++ springboot-redis/redission-demo/src/main/java/com/example/redissondemo/RedissonDemoApplication.java
@@ -1,13 +1,13 @@
-package com.example.redissiondemo;
+package com.example.redissondemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RedissionDemoApplication {
+public class RedissonDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RedissionDemoApplication.class, args);
+        SpringApplication.run(RedissonDemoApplication.class, args);
     }
 
 }
